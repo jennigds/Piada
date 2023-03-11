0e219e42db468d8f3e2f07fcb096868cc39f9cc4
Jenni <e107ti@etec.sp;gov;br>
2023-03-10 22:44:19 -0300

    Resultado

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Na manhã de seu aniversário, uma mulher disse ao marido:");
+Console.WriteLine("- Sonhei que você me dava um colar de diamantes. O que acha que isso significa?");
+Console.WriteLine("- Talvez você descubra hoje à noite, respondeu ele.");
+Console.WriteLine();
+Console.WriteLine("Naquela noite, o homem chegou em casa com um pequeno pacote e o entregou à");
+Console.WriteLine("mulher. Ela rasgou o papel de embrulho, ansiosa, e encontrou um livro: O significado");
+Console.WriteLine("dos sonhos.");
